ddaeb0aec12c40f072b1fd91578a1aebda5826fa
Moamen <mmoamenmohamed101@gmail.com>
2023-09-02 16:16:44 +0300

    name

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,8 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 // comment
+
+  // My name
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
