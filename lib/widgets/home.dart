e7bc2d5f86407c2a284e10d400026f3d1307e527
Manar <manarhamdikamel@gmail.com>
2023-09-13 16:49:30 +0300

    Navigation bar + home + booking

--- lib/widgets/home.dart
+++ lib/widgets/home.dart
@@ -5,13 +5,26 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 class HomeItem extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
+    return Column(crossAxisAlignment: CrossAxisAlignment.start,children: [
       Container(
-        child: TextField(decoration: InputDecoration(label: SearchBar(backgroundColor: [MaterialStatePropertyAll<Color>(value)
-        ], ,)),
+        padding: EdgeInsets.symmetric(horizontal: 40),
+        child: TextField(decoration: InputDecoration(suffixIcon: Icon(Icons.search,color:Color(0xff88C3EA)),border: OutlineInputBorder(borderRadius: BorderRadius.circular(13.0),borderSide: BorderSide(color: Color(0xff88C3EA),width: 1)),label: SearchBar(
+          elevation: MaterialStateProperty.all(0),backgroundColor: MaterialStateProperty.all(Colors.transparent)
+          ,hintText: "Search",
+          hintStyle: MaterialStateProperty.all(TextStyle(color: Color(0xffC6C7D5),fontSize: 16.sp,fontWeight: FontWeight.w500)), )),
           style: TextStyle(fontSize:16.sp,fontWeight: FontWeight.w500 ,color: Color(0xffC6C7D5)),
         ),
-      )
+      ),
+      SizedBox(height: 25.h,),
+      Padding(
+        padding: const EdgeInsets.all(19.0),
+        child: Text("Welcome to our app",style: TextStyle(fontSize: 20.sp,fontWeight: FontWeight.w600,color: Color(0xff322653)),),
+      ),
+      Stack(children: [
+        Container(
+          child: Image.asset("name"),
+        )
+      ],)
     ],);
   }
 }
