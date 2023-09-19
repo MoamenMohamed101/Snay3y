f53fdffa0a4b458c49f6274472cc458dbe472c2f
Manar <manarhamdikamel@gmail.com>
2023-09-19 17:00:05 +0300

    home_ui

---
+++ lib/widgets/client_feed_back_item.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:snay3y/models/client_feedback_model.dart';
+
+class ClientfeedBackItem extends StatelessWidget{
+  final ClientFeedBackModel clientfeedBack;
+  ClientfeedBackItem({required this.clientfeedBack});
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+
+      fit: StackFit.expand,
+      children: [
+        Container(
+
+        ),
+
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children:[
+
+            Text(clientfeedBack.clientnName,style: TextStyle(fontSize: 12.sp , fontWeight: FontWeight.w700)),
+            Text(clientfeedBack.feedBack,style: TextStyle(fontSize: 11.sp , fontWeight: FontWeight.w400))
+          ]),
+        ),
+        Image.asset(clientfeedBack.image,alignment: Alignment.topCenter),
+
+      ],
+    );
+
+  }
+
+}
