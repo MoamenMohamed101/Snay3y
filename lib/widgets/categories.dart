050bf6cab91d8b69f32ffcad9bcce5e4980eb863
Moamen <mmoamenmohamed101@gmail.com>
2023-09-27 14:24:08 +0300

    edits

--- lib/widgets/categories.dart
+++ lib/widgets/categories.dart
@@ -2,15 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class CategoriesScreen extends StatelessWidget{
+  const CategoriesScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Categories",style: TextStyle(color: Colors.black)),
+      appBar: AppBar(title: const Text("Categories",style: TextStyle(color: Colors.black)),
            backgroundColor: Colors.transparent,
         leading:IconButton(onPressed: (){
           Navigator.pop(context);
         },
-            icon: Icon(Icons.arrow_back,color: Colors.black,)) ,
+            icon: const Icon(Icons.arrow_back,color: Colors.black,)) ,
      elevation: 0,
       toolbarHeight: 100.h,
 
